23d3f0178e273311c3fc0b300c0d69ca5ea1e6d9
Vortex <prevett.james@gmail.com>
2023-09-01 16:40:48 +0000

    Added classes

---
+++ src/circuit.ts
@@ -0,0 +1,95 @@
+import { Load, LoadJSON, LoadType, solveLoadUnknowns } from './load';
+
+export enum CircuitType {
+	NONE,
+	SERIES,
+	PARALLEL,
+}
+
+export interface CircuitJSON extends LoadJSON {
+	circuitType: CircuitType;
+	loads: LoadJSON[];
+}
+
+export class Circuit<T extends CircuitType = CircuitType> extends Load<LoadType.CIRCUIT> {
+	loads: Load[];
+
+	get voltage(): number {
+		if (!this._voltage) {
+			this.solve();
+		}
+		return this._voltage;
+	}
+
+	get current(): number {
+		if (!this._current) {
+			this.solve();
+		}
+		return this._current;
+	}
+
+	get resistance(): number {
+		if (!this._resistance) {
+			this.solve();
+		}
+		return this._resistance;
+	}
+
+	private _isSolved: boolean = false;
+	get isSolved(): boolean {
+		return this._isSolved;
+	}
+
+	constructor(public circuitType: T) {
+		super(LoadType.CIRCUIT);
+	}
+
+	solve(): void {
+		if (this._isSolved) {
+			return;
+		}
+		for (const load of this.loads) {
+			if (this.circuitType == CircuitType.SERIES) {
+				this._current ||= load.current;
+			}
+			if (this.circuitType == CircuitType.PARALLEL) {
+				this._voltage ||= load.voltage;
+			}
+			switch (load.loadType) {
+				case LoadType.BASIC:
+					solveLoadUnknowns(load);
+					break;
+				case LoadType.CIRCUIT:
+					if (!(load instanceof Circuit)) {
+						throw new TypeError('Load with type circuit is not a circuit');
+					}
+
+					load.solve();
+			}
+			if (this.circuitType == CircuitType.SERIES) {
+				this._voltage += load.voltage;
+				this._resistance += load.resistance;
+			}
+			if (this.circuitType == CircuitType.PARALLEL) {
+				this._current += load.current;
+			}
+		}
+		if (this.circuitType == CircuitType.PARALLEL) {
+			this._resistance = this._voltage / this._current;
+		}
+		this._isSolved = true;
+	}
+
+	toJSON(): CircuitJSON {
+		return Object.assign(super.toJSON(), {
+			loads: this.loads.map(load => load.toJSON()),
+			circuitType: this.circuitType
+		});
+	}
+
+	static FromJSON(json: CircuitJSON, circuit: Circuit = new Circuit(CircuitType.NONE)): Circuit {
+		super.FromJSON(json, circuit);
+		circuit.circuitType = json.circuitType;
+		return circuit;
+	}
+}
---
+++ src/load.ts
@@ -0,0 +1,71 @@
+export enum LoadType {
+	BASIC,
+	CIRCUIT,
+}
+
+export interface LoadJSON {
+	voltage?: number;
+	current?: number;
+	resistance?: number;
+}
+
+export class Load<T extends LoadType = LoadType> implements LoadJSON {
+	protected _voltage: number = 0;
+	protected _current: number = 0;
+	protected _resistance: number = 0;
+	get voltage(): number {
+		return this._voltage;
+	}
+	set voltage(value: number) {
+		this._voltage = value;
+	}
+	get current(): number {
+		return this._current;
+	}
+	set current(value: number) {
+		this._current = value;
+	}
+	get resistance(): number {
+		return this._resistance;
+	}
+	set resistance(value: number) {
+		this._resistance = value;
+	}
+
+	get loadType(): T {
+		return this._loadType;
+	}
+	constructor(protected _loadType: T) {}
+
+	toJSON(): LoadJSON {
+		return {
+			voltage: this.voltage,
+			current: this.current,
+			resistance: this.resistance,
+		}
+	}
+
+	static FromJSON(json: LoadJSON, load: Load = new Load(LoadType.BASIC)): Load {
+		load.voltage = json.voltage;
+		load.current = json.current;
+		load.resistance = json.resistance;
+		return load;
+	}
+}
+
+export function solveLoadUnknowns<T extends LoadType>(load: LoadJSON): Load<T> {
+
+	if (typeof load.current == 'number' && typeof load.resistance == 'number') {
+		load.voltage = load.current * load.resistance;
+	}
+
+	if (typeof load.voltage == 'number' && typeof load.resistance == 'number') {
+		load.current = load.voltage / load.resistance;
+	}
+
+	if (typeof load.voltage == 'number' && typeof load.current == 'number') {
+		load.resistance = load.voltage / load.current;
+	}
+
+	return load as Load<T>;
+}
